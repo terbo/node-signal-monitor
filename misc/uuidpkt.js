308a3f890e1df5c27a8f3f54b25c61824e1e23fb
root <terry.chad@gmail.com>
2019-10-29 15:15:16 -0700

    add stop function, formatting

--- misc/uuidpkt.js
+++ misc/uuidpkt.js
@@ -6,6 +6,12 @@ const pcap = require('pcap'),
 	    sha1 = require('sha1'),
       process = require('process')
 
+const dot11_beacon_ie_fingerprint=['0,1,45,48,50,61,74,127,191,195,221-00156D-00,221-0050F2-2,221-001018-2,221-506F9A-28']
+const dot11_probe_ie_fingerprint=['1,50,59,107,127,221-001018-2,221-00904c-51']
+
+var lastStatus = new Date()
+var changed = {}
+
 if(process.argv.length < 3) {
   console.error('$0 interface')
   process.exit(2)
@@ -13,51 +19,78 @@ if(process.argv.length < 3) {
 
 var uuid,
     sniffer = pcap.createSession(process.argv[2])
-var ids = []
+
+var db = {};
 
 function uuid_from_tags(p) {
-	var type, tags = '', tmp = []
+	var type, tags = '', tmp = [],
+      ssid, fp = '', uuid = ''
+  var pkt, frame, mac
 
   try{
-		const pkt = pcap.decode(p),
-        frame = pkt.payload.ieee802_11Frame,
+		pkt = pcap.decode(p)
+        frame = pkt.payload.ieee802_11Frame
           mac = frame.shost.toString()
-  
+  } catch(e) { return }
+
   if(frame.hasOwnProperty('beacon') && frame.beacon !== undefined) {
     tags = frame.beacon.tags
     type = 'ap'
+    ssid = frame.beacon.tags[0].ssid
+    fp=dot11_beacon_ie_fingerprint
   } else
   if (frame.hasOwnProperty('probe') && frame.probe !== undefined) {
-    tags = pkt.probe.tags
-    type = 'st'
-  } else
-    return
-  
-  if(tags) {
-    Object.keys(tags).forEach(tag => {
-      var tagVal = String(tags[tag].value).trim()
-      
-      if(tagVal && tagVal.length)
-        tmp.push(tagVal)
-    })
-    
-    uuid = sha1(tmp.join())
-    //console.info(mac, uuid)
-  }
-  /*if(uuid in ids) {
-    if(ids[uuid] == mac)
-      console.log(`${mac} => ${uuid}`)
-    else
-      console.log(`${mac} -- ${uuid}`)
+    tags = frame.probe.tags
+    type = 'sta'
+    ssid = frame.probe.tags[0].ssid
+    fp=dot11_probe_ie_fingerprint
   } else {
-    ids[uuid] = mac
-  }*/
-  console.log(`\t(${type}) ${mac}  =>  ${uuid}  - ${tags.length}\n`)
-	} catch(e){
-   //console.log(e)
-		return
+    console.log('return')
+    return
   }
 
+  if(tags != '') {
+    try {
+      fp.forEach(key => {
+        var tagVal = tags[key]
+        
+        if(tagVal){
+          console.log(`${tagVal} ${key}`)
+          tmp.push(tagVal)
+        } 
+      })
+
+    if(tmp.length)
+      uuid = sha1(tmp.join())
+    else {
+      console.log(tmp)
+      return
+    }
+  
+    if(uuid == '')
+      return
+
+    } catch(e) { console.log(e) }
+        
+    
+    if(db.hasOwnProperty(uuid)) {
+      if((db[uuid].mac != mac) || (db[uuid].ssid != ssid)) {
+        if(changed.hasOwnProperty(uuid))
+          changed[uuid].push((new Date(),tmp.length))
+        else
+          changed[uuid] = [(new Date(),tmp.length)]
+      }
+    } else {
+      //console.log({mac, ssid})
+      db[uuid] = {mac, ssid, tags: tmp.length}
+    }
+  }
+
+  if(new Date() - lastStatus > 5000) {
+    lastStatus = new Date()
+    console.dir(db)
+    console.dir(changed)
+  }
 }
 
 // now just need the packets that kizmet marks
